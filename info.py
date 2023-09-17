603d76e49ed278e72492c6069ba1da73346d8ac4
Mr. Anonymous 2.0 <143740148+MKanchan2023@users.noreply.github.com>
2023-09-17 19:50:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Movie_files')
 
 # Others
 VERIFY = is_enabled((environ.get("VERIFY", "False")), False)
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mplaylink.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '09aa271212a68057b9b038672a0e91968118e23a')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'dJY2xHc074eCI6gCqkV7LNVXntf1')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
